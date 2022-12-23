6f759f0d207e71053126463028a054580611b658
Takatada Yoshima <yoshima@fulldrive.jp>
2022-12-23 22:13:23 +0900

    Update abc090_b

--- abc090/b.py
+++ abc090/b.py
@@ -2,7 +2,7 @@ A, B = map(int, input().split())
 
 cnt = 0
 for i in range(A, B+1):
-    istr = str(i)
-    if istr[0] == istr[4] and istr[1] == istr[3]:
+    if i % 10 == int(i / 10000) and int(i / 10) % 10 == int(i / 1000) % 10:
         cnt += 1
+
 print(cnt)
