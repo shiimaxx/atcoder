869ce1adcba7eeb5718531c80d59d8ac22b3d368
Takatada Yoshima <yoshima@fulldrive.jp>
2023-01-07 10:14:55 +0900

    Revert "temporary"
    This reverts commit 469a941a53df9a28b76e2253a5da59dafc5c502b.

--- abc112/c.py
+++ abc112/c.py
@@ -1,9 +1,28 @@
 N = int(input())
-coordinates = [[0 for _ in range(10)] for _ in range(10)]
+coordinates = []
 
 for _ in range(N):
     x, y, h = map(int, input().split())
-    coordinates[x][y] = h
+    coordinates.append([x, y, h])
 
-for c in coordinates:
-    print(c)
+bucket = {}
+for X in range(101):
+    for Y in range(101):
+        for H in range(200):
+            for c in coordinates:
+                x, y, h = c[0], c[1], c[2]
+                if h == max([H - abs(X - x) - abs(Y - y), 0]):
+                    key = f"{X}_{Y}_{H}"
+                    if key not in bucket:
+                        bucket[key] = 1
+                    else:
+                        bucket[key] += 1
+
+max = 0
+max_k = ''
+for k, v in bucket.items():
+    if max == 0 or max < v:
+        max = v
+        max_k = k
+
+print(' '.join(max_k.split('_')))
