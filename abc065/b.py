aa2c9d8e41219c6251327da8c68cec9c3d56e5d6
Takatada Yoshima <yoshima@fulldrive.jp>
2023-01-30 22:01:16 +0900

    abc065_b AC

--- abc065/b.py
+++ abc065/b.py
@@ -1,15 +1,15 @@
 N = int(input())
 a = [int(input()) for _ in range(N)]
+hist = [0 for _ in range(N)]
 
 cnt = 0
 b = 1
-hist = [b]
 while b != 2:
     b = a[b-1]
-    if b in hist:
+    if hist[b-1] == 1:
         cnt = -1
         break
     cnt += 1
-    hist.append(b)
+    hist[b-1] = 1
 
 print(cnt)
