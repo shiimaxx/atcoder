d248e812686c0ee8b1d9572b517b0fef2dfe2968
Takatada Yoshima <yoshima@fulldrive.jp>
2023-03-11 11:48:31 +0900

    joi2011yo_d

--- joi2011yo/d.py
+++ joi2011yo/d.py
@@ -1,22 +1,26 @@
+import itertools
+
 n = int(input())
 k = int(input())
 c = [input() for _ in range(n)]
 b = {}
 
 
-def f(i, ci, num_cards):
-    if num_cards >= k:
-        if ci not in b:
-            b[ci] = 0
-        return
-    for j in range(n):
-        if i == j:
-            continue
-        f(i, ci + c[j], num_cards+1)
+def f(draw):
+    for p in itertools.permutations(draw):
+        s = ''
+        for pp in p:
+            s += c[pp]
+        if s not in b:
+            b[s] = 0
 
 
-for i in range(n):
-    f(i, c[i], 1)
+for i in range(2 ** n):
+    if bin(i).count("1") == k:
+        draw = []
+        for j in range(n):
+            if ((i >> j) & 1):
+                draw.append(j)
+        f(draw)
 
-print(b)
 print(len(b.keys()))
